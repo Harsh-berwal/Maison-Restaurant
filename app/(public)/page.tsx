fa160d47b993695f5e9fa0e620b36f06214f1486
Harsh <harshberwal240@gmail.com>
2026-06-26 09:58:46 +0530

    Intialized project

---
+++ app/(public)/page.tsx
@@ -0,0 +1,22 @@
+import Navbar from "@/components/layouts/navbar";
+
+// import About from "@/components/home/About";
+// import FeaturedMenu from "@/components/home/FeaturedMenu";
+// import Testimonials from "@/components/home/Testimonials";
+// import CTA from "@/components/home/CTA";
+// import Footer from "@/components/layouts/footer";
+
+
+export default function Home() {
+  return (
+    <>
+      <Navbar />
+      {/* <About />
+     <Hero />
+      <FeaturedMenu />
+      <Testimonials />
+      <CTA />
+      <Footer /> */}
+    </>
+  );
+}
---
+++ components/layouts/navbar.tsx
@@ -0,0 +1,9 @@
+export default function Navbar() {
+  return (
+    <nav className="w-full h-24 bg-green-500">
+      <div className="ml-auto mr-auto w-11/12 border-4 border-red-500 bg-black">
+        <p className="text-white">Centered?</p>
+      </div>
+    </nav>
+  );
+}
